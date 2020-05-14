3d01710edacc1eb42d70e36ab1ac97a57f43c820
Txomas <txomas.vf@gmail.com>
2020-05-14 15:24:59 +0300

    Working version

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ColumnHeader columnHeader1;
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Objects");
             this.comboBox = new System.Windows.Forms.ComboBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.panel = new System.Windows.Forms.Panel();
@@ -39,30 +39,30 @@
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.listView = new System.Windows.Forms.ListView();
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // columnHeader1
-            // 
-            columnHeader1.Text = "Objects";
-            columnHeader1.Width = 388;
-            // 
             // comboBox
             // 
+            this.comboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox.FormattingEnabled = true;
-            this.comboBox.Location = new System.Drawing.Point(3, 3);
+            this.comboBox.Location = new System.Drawing.Point(16, 73);
             this.comboBox.Name = "comboBox";
-            this.comboBox.Size = new System.Drawing.Size(151, 24);
+            this.comboBox.Size = new System.Drawing.Size(271, 46);
             this.comboBox.TabIndex = 0;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(21, 33);
+            this.btnAdd.Enabled = false;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.Location = new System.Drawing.Point(6, 247);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(121, 23);
+            this.btnAdd.Size = new System.Drawing.Size(145, 58);
             this.btnAdd.TabIndex = 1;
             this.btnAdd.Text = "Create";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -70,6 +70,7 @@
             // 
             // panel
             // 
+            this.panel.Controls.Add(this.label3);
             this.panel.Controls.Add(this.label2);
             this.panel.Controls.Add(this.textBoxName);
             this.panel.Controls.Add(this.comboBoxValue);
@@ -79,102 +80,121 @@
             this.panel.Controls.Add(this.label1);
             this.panel.Controls.Add(this.comboBox);
             this.panel.Controls.Add(this.btnAdd);
-            this.panel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel.Location = new System.Drawing.Point(0, 525);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel.Location = new System.Drawing.Point(466, 0);
             this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(780, 65);
+            this.panel.Size = new System.Drawing.Size(299, 590);
             this.panel.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(418, 7);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(36, 370);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 17);
+            this.label2.Size = new System.Drawing.Size(245, 39);
             this.label2.TabIndex = 9;
             this.label2.Text = "Property value:";
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(258, 5);
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxName.Location = new System.Drawing.Point(16, 184);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(133, 22);
+            this.textBoxName.Size = new System.Drawing.Size(271, 45);
             this.textBoxName.TabIndex = 8;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
             // 
             // comboBoxValue
             // 
+            this.comboBoxValue.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxValue.Enabled = false;
+            this.comboBoxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBoxValue.FormattingEnabled = true;
-            this.comboBoxValue.Location = new System.Drawing.Point(551, 4);
+            this.comboBoxValue.Location = new System.Drawing.Point(16, 430);
             this.comboBoxValue.Name = "comboBoxValue";
-            this.comboBoxValue.Size = new System.Drawing.Size(190, 24);
+            this.comboBoxValue.Size = new System.Drawing.Size(271, 46);
             this.comboBoxValue.TabIndex = 7;
             this.comboBoxValue.Visible = false;
             // 
             // textBoxValue
             // 
-            this.textBoxValue.Location = new System.Drawing.Point(551, 3);
+            this.textBoxValue.Enabled = false;
+            this.textBoxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxValue.Location = new System.Drawing.Point(43, 430);
             this.textBoxValue.Name = "textBoxValue";
-            this.textBoxValue.Size = new System.Drawing.Size(190, 22);
+            this.textBoxValue.Size = new System.Drawing.Size(228, 45);
             this.textBoxValue.TabIndex = 6;
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(512, 32);
+            this.btnEdit.Enabled = false;
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnEdit.Location = new System.Drawing.Point(95, 493);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(155, 23);
+            this.btnEdit.Size = new System.Drawing.Size(130, 58);
             this.btnEdit.TabIndex = 5;
             this.btnEdit.Text = "Edit";
             this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(207, 33);
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDelete.Location = new System.Drawing.Point(157, 247);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(141, 23);
+            this.btnDelete.Size = new System.Drawing.Size(135, 58);
             this.btnDelete.TabIndex = 4;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(160, 7);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(36, 131);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.Size = new System.Drawing.Size(218, 39);
             this.label1.TabIndex = 2;
             this.label1.Text = "Object name:";
             // 
-            // listView
+            // treeView
             // 
-            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            columnHeader1,
-            this.columnHeader2});
-            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.listView.GridLines = true;
-            this.listView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.listView.HideSelection = false;
-            this.listView.Location = new System.Drawing.Point(0, 0);
-            this.listView.MultiSelect = false;
-            this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(780, 525);
-            this.listView.TabIndex = 3;
-            this.listView.UseCompatibleStateImageBehavior = false;
-            this.listView.View = System.Windows.Forms.View.Details;
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView.HideSelection = false;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Objects";
+            this.treeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView.Size = new System.Drawing.Size(466, 590);
+            this.treeView.TabIndex = 3;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            this.treeView.Click += new System.EventHandler(this.treeView_Click);
             // 
-            // columnHeader2
+            // label3
             // 
-            this.columnHeader2.Text = "Properties";
-            this.columnHeader2.Width = 388;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(36, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 39);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Choose class:";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(780, 590);
-            this.Controls.Add(this.listView);
+            this.ClientSize = new System.Drawing.Size(765, 590);
+            this.Controls.Add(this.treeView);
             this.Controls.Add(this.panel);
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form";
             this.panel.ResumeLayout(false);
             this.panel.PerformLayout();
@@ -193,9 +213,9 @@
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listView;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -13,28 +13,158 @@ namespace lab_2
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+         public MainForm()
         {
             InitializeComponent();
 
-            listView.Columns[0].Width = listView.Size.Width / 2;
-            listView.Columns[1].Width = -2;
-
             Assembly Asm = Assembly.LoadFrom("CRUD_lib.dll");
-            Type[] Types = Asm.GetTypes();
-            foreach (Type t in Types)
+            List<Type> Types = new List<Type>();
+            foreach (Type t in Asm.GetTypes())
             {
                 if (!t.IsAbstract)
-                    comboBox.Items.Add(t.Name);
+                    Types.Add(t);
             }
+            comboBox.DataSource = Types;
+            comboBox.DisplayMember = "Name";
             comboBox.SelectedItem = comboBox.Items[0];
+        }
 
-
+        private void comboBoxValue_Fill()
+        {
+            comboBoxValue.Items.Clear();
+            foreach (TreeNode T in treeView.Nodes[0].Nodes)
+            {
+                if ((Type)T.Tag == ((FieldInfo)treeView.SelectedNode.Tag).FieldType) 
+                {
+                    comboBoxValue.Items.Add(T.Text);
+                }
+            }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            treeView.Nodes[0].Nodes.Add(new TreeNode(textBoxName.Text));
+            Type t = (Type)comboBox.SelectedItem;
+            FieldInfo[] Fields = t.GetFields();
+            foreach (FieldInfo f in Fields)
+            {
+                treeView.Nodes[0].LastNode.Nodes.Add(new TreeNode(f.Name + ": "));
+                treeView.Nodes[0].LastNode.LastNode.Tag = f;
+                treeView.Nodes[0].LastNode.Tag = comboBox.SelectedItem;
+            }
+            treeView.Nodes[0].ExpandAll();
+            if (comboBoxValue.Enabled == true)
+            {
+                comboBoxValue_Fill();
+            }
+            textBoxName.Clear();
+        }
 
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (((Type)treeView.SelectedNode.Tag).Namespace == "CRUD_lib") //Object selected, Is CRUD-class
+            {
+                foreach (TreeNode T in treeView.Nodes[0].Nodes) //Look through objects
+                {
+                    foreach (TreeNode P in T.Nodes) //Look through properties
+                    {
+                        if (P.Text == ((FieldInfo)P.Tag).Name + ": " + treeView.SelectedNode.Text) //Is property-node value equals object-node text
+                        {
+                            P.Text = ((FieldInfo)P.Tag).Name; //Clear property value
+                        }
+                    }
+                }
+            }
+            treeView.Nodes.Remove(treeView.SelectedNode);
+            btnDelete.Enabled = false;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (textBoxValue.Visible == true)
+            {
+                treeView.SelectedNode.Text = ((FieldInfo)treeView.SelectedNode.Tag).Name + ": " + textBoxValue.Text;
+            }
+            else
+            {
+                treeView.SelectedNode.Text = ((FieldInfo)treeView.SelectedNode.Tag).Name + ": " + comboBoxValue.Text;
+            }
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (treeView.Focused == true)
+            {
+                if (treeView.SelectedNode.Level == 2) //Is property selected
+                {
+                    btnEdit.Enabled = true;
+                    if (((FieldInfo)treeView.SelectedNode.Tag).FieldType.Namespace == "CRUD_lib") //Is class-property selected
+                    {
+                        textBoxValue.Visible = false;
+                        comboBoxValue.Visible = true;
+                        comboBoxValue.Enabled = true;
+                        comboBoxValue_Fill();
+                    }
+                    else
+                    {
+                        textBoxValue.Visible = true;
+                        textBoxValue.Enabled = true;
+                        comboBoxValue.Visible = false;
+                    }
+                }
+                else
+                {
+                    btnEdit.Enabled = false;
+                    comboBoxValue.Enabled = false;
+                    textBoxValue.Enabled = false;
+                }
+
+                btnDelete.Enabled = treeView.SelectedNode.Level == 1 ? true : false;
+            }
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxName.Text == "")
+            {
+                btnAdd.Enabled = false;
+            }
+            else
+            {
+                btnAdd.Enabled = true;
+            }
+        }
+
+        private void treeView_Click(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode != null)
+            {
+                if (treeView.SelectedNode.Level == 2) //Is property selected
+                {
+                    btnEdit.Enabled = true;
+                    if (((FieldInfo)treeView.SelectedNode.Tag).FieldType.Namespace == "CRUD_lib") //Is class-property selected
+                    {
+                        textBoxValue.Visible = false;
+                        comboBoxValue.Visible = true;
+                        comboBoxValue.Enabled = true;
+                        comboBoxValue_Fill();
+                    }
+                    else
+                    {
+                        textBoxValue.Visible = true;
+                        textBoxValue.Enabled = true;
+                        comboBoxValue.Visible = false;
+                    }
+                }
+                else
+                {
+                    btnEdit.Enabled = false;
+                    comboBoxValue.Enabled = false;
+                    textBoxValue.Enabled = false;
+                }
+
+                btnDelete.Enabled = treeView.SelectedNode.Level == 1 ? true : false;
+            }
         }
     }
 }
